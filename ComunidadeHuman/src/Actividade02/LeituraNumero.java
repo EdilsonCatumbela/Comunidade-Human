2491b75c495ce0532d43a5b6f1f09360112ce378
Programador <Programador@CATUMBELA.mshome.net>
2021-05-03 16:59:36 +0100

    Resolvendo actividade 02

---
+++ ComunidadeHuman/src/Actividade02/LeituraNumero.java
@@ -0,0 +1,20 @@
+package Actividade02;
+
+import java.util.Scanner;
+
+public class LeituraNumero {
+private static Scanner scan;
+	public static void main(String[] args) {
+		int numero;
+		scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número");
+		numero = scan.nextInt();
+		
+		System.out.println("O número que você digitou é " + numero);
+		
+		
+
+	}
+
+}
