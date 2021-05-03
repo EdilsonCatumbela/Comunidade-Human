cda5a1d5c8537d1d549f5babd5894fe277cdc7a8
Programador <Programador@CATUMBELA>
2021-05-03 23:44:44 +0100

    Resolvendo Actividade08

--- ComunidadeHuman/src/Actividade08/SalarioTotal.java
+++ ComunidadeHuman/src/Actividade08/SalarioTotal.java
@@ -1,4 +1,4 @@
-package Activadade08;
+package Actividade08;
 /*
  * 8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
  */
